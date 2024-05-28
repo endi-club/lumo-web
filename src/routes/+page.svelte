e7fe7dda6c71d7b809abe417a809824ba220c44e
Perny <pernydev@gmail.com>
2024-05-28 23:21:55 +0300

    Add header section with button component to main page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<header class="mx-auto mt-16 max-w-[800px] text-center">
+	<h1 class="text-8xl font-black font-apply">Äärettömän <i class="text-secondary">edullinen</i> tapa tukea</h1>
+	<Button class="mt-12 p-6 text-2xl font-bold">Tilaa nyt!</Button>
+</header>
+
+<style>
+</style>
